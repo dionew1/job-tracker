f63fdc1a2087b5068f9e5570abf9241c910acade
Dione Wilson <dionew1@gmail.com>
2017-10-16 21:38:36 -0600

    complete show for category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show, :edit, :update, :destroy]
 
   def index
     @categories = Category.all
@@ -18,10 +19,16 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def show
+  end
+
   private
 
     def category_params
       params.require(:category).permit(:title)
     end
 
+    def set_category
+      @category = Category.find(params[:id])
+    end
 end
