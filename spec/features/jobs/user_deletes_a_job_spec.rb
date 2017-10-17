1db70cd6c4b1c0dcc2e74287c8aa12cb0bf44612
Dione Wilson <dionew1@gmail.com>
2017-10-16 19:58:21 -0600

    Passing tests

--- spec/features/jobs/user_deletes_a_job_spec.rb
+++ spec/features/jobs/user_deletes_a_job_spec.rb
@@ -1,16 +1,17 @@
 require 'rails_helper'
 
 describe "User deletes existing job" do
-  scenario "a user can delete a company" do
+  scenario "a user can delete a job" do
     company = Company.create(name: "ESPN")
-    Job.create!(title: "Q&A", description: "Testing", level_of_interest: "50", city: "Colorado Springs", company: company)
+    job = Job.create!(title: "Q&A", description: "Testing", level_of_interest: "50", city: "Colorado Springs", company: company)
 
     visit company_job_path(company, job)
 
-    within(".company_#{company.id}_job_#{job.id}") do
-      click_link "Delete"
-    end
+    click_on "Delete"
+    # within(".company_#{company.id}_job_#{job.id}") do
+    #   click_link "Delete"
+    # end
 
-    expect(page).to have_content("Q&A was successfully deleted!")
+    expect(page).to have_content("Q&A at ESPN was successfully deleted!")
   end
 end
